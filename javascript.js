fc8111d71b9e7f7a1d74fc93b902484556c4a621
Tomi Kuusik <tomikuusik@gmail.com>
2018-11-01 22:28:31 +0200

    finished project

--- javascript.js
+++ javascript.js
@@ -1,6 +1,31 @@
 const containerDiv = document.querySelector('#maindiv');
-for (i = 0; i < (16*16); i++) {
-    const div = document.createElement('div');
-    div.id = "div-" + (i+1);
-    containerDiv.appendChild(div);
+document.querySelector('#resetButton').addEventListener('click', () => {initiateGrid(Math.floor(prompt("How many grids?")))});
+function resetGame () {
+    let promptValue = Math.floor(prompt("How many grids?"));
+    promptValue < 26 ? initiateGrid(promptValue) : alert("Amount of boxes on one axis has to be smaller than 26") 
 }
+initiateGrid(16);
+function initiateGrid (axisBoxes) {
+    containerDiv.innerHTML = "";
+    let boxMargin = Math.floor(axisBoxes*(-0.4166) + 11.666);
+    boxMargin <= 0 ? boxMargin = 1 : null ;
+    let widthBox = (Math.floor(((800 - boxMargin*2*axisBoxes) / axisBoxes)*10))/10;
+    for (i = 0; i < (Math.pow(axisBoxes, 2)); i++) {
+        const div = document.createElement('div');
+        containerDiv.appendChild(div);
+        containerDiv.lastChild.setAttribute("style", "width: " + widthBox + "px; height: " + widthBox + "px" + "; margin: " + boxMargin + "px");
+    }
+}
+$("#centerDiv").hover(
+    function random_bg_color() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        $("#mainDiv div").hover(function () { 
+            $(this).css("background-color", bgColor);
+        }, function () {
+            $(this).css("background-color", "gainsboro");
+        });
+    }
+);
